5a82efe89912c159d1fd7dd318c2214c077c036e
MiguelSDA <miguelitojurado1203@gmail.com>
2024-05-28 22:40:20 -0500

    break

---
+++ Juego BreakOut/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Juego_BreakOut
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtScore = new Label();
+            player = new PictureBox();
+            ball = new PictureBox();
+            gameTImer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // txtScore
+            // 
+            txtScore.AutoSize = true;
+            txtScore.BackColor = Color.Black;
+            txtScore.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
+            txtScore.ForeColor = Color.White;
+            txtScore.Location = new Point(17, 13);
+            txtScore.Margin = new Padding(4, 0, 4, 0);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(110, 29);
+            txtScore.TabIndex = 0;
+            txtScore.Text = "Score: 0";
+            // 
+            // player
+            // 
+            player.BackColor = Color.White;
+            player.Location = new Point(463, 606);
+            player.Margin = new Padding(4, 3, 4, 3);
+            player.Name = "player";
+            player.Size = new Size(133, 39);
+            player.TabIndex = 1;
+            player.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.BackColor = Color.Yellow;
+            ball.Location = new Point(503, 366);
+            ball.Margin = new Padding(4);
+            ball.Name = "ball";
+            ball.Size = new Size(33, 28);
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            // 
+            // gameTImer
+            // 
+            gameTImer.Interval = 20;
+            gameTImer.Tick += mainGameTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(1067, 660);
+            Controls.Add(ball);
+            Controls.Add(player);
+            Controls.Add(txtScore);
+            Font = new Font("Microsoft Sans Serif", 7.8F);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += keyisdown;
+            KeyUp += keyisup;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label txtScore;
+        private PictureBox player;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer gameTImer;
+    }
+}
